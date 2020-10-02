7dfca33bb6eda9fae15510188e0e6a2bce7c71d0
sergiomartindg <sergiomartindg@gmail.com>
2020-10-02 20:13:40 +0200

    add files

---
+++ Celda/Cell.py
@@ -0,0 +1,23 @@
+
+class Cell:
+    def __init__(self, row, column, value, neighbors):
+        #fila y columna en la que se encuentra la celda, podria ponerse como tupla
+        self.row = row
+        self.column = column
+        self.value = value
+        self.neighbors = neighbors
+
+    def get_row (self):
+        return self.row
+
+    def get_column(self):
+        return self.column
+
+    def get_value(self):
+        return self.value
+
+    def get_neighbors(self):
+        return self.neighbors
+
+    def to_string(self):
+        return ("Fila: '{0}' \nColumna: '{1}' \nValor: '{2}' \nVecinos: '{3}' \n".format(self.row, self.column, self.value, self.neighbors))
---
+++ Laberinto/Labyrinth.py
@@ -0,0 +1,75 @@
+import json
+
+from Celda.Cell import Cell
+import cnfg
+import numpy as np
+import pygame
+
+
+class Labyrinth:
+    def __init__(self, path):
+        try:
+            with open(path) as file:
+                self.dict_data = json.load(file)
+                self.labyrinth = None
+
+        except FileNotFoundError:
+            print("Error. File '{0}' not found".format(path))
+            exit()
+
+    def get_rows(self):
+        return int(self.dict_data["rows"])
+
+    def get_cols(self):
+        return int(self.dict_data["cols"])
+
+    def get_labyrinth(self):
+        return self.labyrinth
+
+    def create_labyrinth(self):
+        self.labyrinth = np.empty([int(self.dict_data["rows"]), int(self.dict_data["cols"])], dtype=object)
+
+    def load_data(self):
+        cells = (self.dict_data["cells"])
+
+        self.create_labyrinth()
+
+        for i in range(0, self.dict_data["rows"]):
+            for j in range(0, self.dict_data["cols"]):
+                coordenadas = cells["({0}, {1})".format(i, j)]
+                cell = Cell(i, j, coordenadas["value"], coordenadas["neighbors"])
+                self.labyrinth[i][j] = cell
+                # Solo para comprobar que se crea, aqui no deberia ir
+                # print(cell.to_string())
+
+    def dibujar(self, screen):
+        if self.dict_data["rows"] > self.dict_data["cols"]:
+            w = int(cnfg.ancho / self.dict_data["rows"])
+        else:
+            w = int(cnfg.alto / self.dict_data["cols"])
+
+        y = -w + 20
+
+        for i in range(0, self.dict_data["rows"]):
+            # set x coordinate to start position
+            x = 20
+            y = y + w
+            for j in range(0, self.dict_data["cols"]):
+                cell = self.labyrinth[i][j]
+                vecinos = cell.get_neighbors()
+                # top of cell
+                if vecinos[0] == False:
+                    pygame.draw.line(screen, cnfg.BLACK, [x, y], [x + w, y])
+
+                # right of cell
+                if vecinos[1] == False:
+                    pygame.draw.line(screen, cnfg.BLACK, [x + w, y], [x + w, y + w])
+
+                # bottom of cell
+                if vecinos[2] == False:
+                    pygame.draw.line(screen, cnfg.BLACK, [x + w, y + w], [x, y + w])
+
+                # left of cell
+                if vecinos[3] == False:
+                    pygame.draw.line(screen, cnfg.BLACK, [x, y + w], [x, y])
+                x = x + w
