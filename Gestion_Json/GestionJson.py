d6fa56e875d201b9c9f9d665164ddb308b99d9a3
jalvarezz13 <javier.alvarez13@alu.uclm.es>
2020-10-14 19:12:53 +0200

    Comprobación correcta de JSONs en laberintos de tamaño nxn COMPLETADO

--- Gestion_Json/GestionJson.py
+++ Gestion_Json/GestionJson.py
@@ -82,16 +82,16 @@ class GestionJson:
     def check_json(diccionario):        
         token = True
         dict_for = {1:0, 2:1, 3:2}       
-        for n in range(0, diccionario["rows"]-1):            
-            if(diccionario["cells"]["(0, {0})".format(n)]["neighbors"][0] == True or            
-                diccionario["cells"]["({0}, 0)".format(n)]["neighbors"][3] == True or            
-                diccionario["cells"]["({0}, {1})".format(diccionario["rows"]-1, n)]["neighbors"][2] == True or            
-                diccionario["cells"]["({0}, {1})".format(n, diccionario["cols"]-1)]["neighbors"][2] == True):                
-                    token = False  
+        for n in range(0, diccionario["rows"]):            
+            if(diccionario["cells"]["(0, {0})".format(n)]["neighbors"][0] == True or 
+            diccionario["cells"]["({0}, 0)".format(n)]["neighbors"][3] == True or 
+            diccionario["cells"]["({0}, {1})".format(diccionario["rows"]-1, n)]["neighbors"][2] == True or 
+            diccionario["cells"]["({0}, {1})".format(n, diccionario["cols"]-1)]["neighbors"][1] == True): 
+                token = False  
 
-            for i, j in dict_for.items():                
-                if(diccionario["cells"]["({0}, {1})".format(j, n)]["neighbors"][2] != diccionario["cells"]["({0}, {1})".format(i,n)]["neighbors"][0] or                 
-                    diccionario["cells"]["({0}, {1})".format(n, j)]["neighbors"][1] != diccionario["cells"]["({0}, {1})".format(n,i)]["neighbors"][3]):                    
+            for i in range (1, diccionario["rows"]):                
+                if(diccionario["cells"]["({0}, {1})".format(i-1, n)]["neighbors"][2] != diccionario["cells"]["({0}, {1})".format(i,n)]["neighbors"][0] or                 
+                    diccionario["cells"]["({0}, {1})".format(n, i-1)]["neighbors"][1] != diccionario["cells"]["({0}, {1})".format(n,i)]["neighbors"][3]):                    
                     token = False                            
                 
         if(token == False):            
