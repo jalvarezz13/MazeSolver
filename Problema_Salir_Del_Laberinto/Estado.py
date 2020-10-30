8b0feb2d531dc515f38a47311807665a2c9ad4ce
jalvarezz13 <javier.alvarez13@alu.uclm.es>
2020-10-30 09:45:30 +0100

    Modificación de la clase Estado

--- Problema_Salir_Del_Laberinto/Estado.py
+++ Problema_Salir_Del_Laberinto/Estado.py
@@ -1,16 +1,6 @@
 class Estado:
     def __init__(self, fila, columna, mov, costeMovimiento):
         self.__id = (self.fila, self.columna)
-        self.__mov = mov
-        self.__CosteMovimiento = costeMovimiento
 
     def getId(self):
         return self.__id
-
-    def getMov(self):
-        return self.__mov
-    
-    def getCosto(self):
-        return self.__CosteMovimiento
-
-    
