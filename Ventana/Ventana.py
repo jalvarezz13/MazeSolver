14d9a752bd62cb00db40ff6825729f1ca6e31792
jalvarezz13 <javier.alvarez13@alu.uclm.es>
2020-11-10 17:44:50 +0100

    Ajuste del grosor de linea a la hora de dibujar

--- Ventana/Ventana.py
+++ Ventana/Ventana.py
@@ -1,65 +1,66 @@
 import pygame
 import Cnfg
 
+def dibujarLineas(screen, lab):
+    if lab.get_rows() > lab.get_cols():
+        w = int(Cnfg.ancho / lab.get_rows())
+    else:
+        w = int(Cnfg.alto / lab.get_cols())
+    y = -w + 20
+
+    if(lab.get_rows() >= 75 or lab.get_cols() >= 75):
+        Cnfg.anchoLinea -= 1
+
+    for i in range(0, lab.get_rows()):
+        x = 20
+        y = y + w
+
+        for j in range(0, lab.get_cols()):
+            cell = lab.labyrinth[i][j]
+            vecinos = cell.get_neighbors()
+
+            if not vecinos[0]:
+                pygame.draw.line(screen, Cnfg.BLACK, [x, y], [x + w, y], Cnfg.anchoLinea)
+
+            if not vecinos[1]:
+                pygame.draw.line(screen, Cnfg.BLACK, [x + w, y], [x + w, y + w], Cnfg.anchoLinea)
+
+            if not vecinos[2]:
+                pygame.draw.line(screen, Cnfg.BLACK, [x + w, y + w], [x, y + w], Cnfg.anchoLinea)
+
+            if not vecinos[3]:
+                pygame.draw.line(screen, Cnfg.BLACK, [x, y + w], [x, y], Cnfg.anchoLinea)
+            x = x + w
+
+def dibujarColores(screen, lab):
+    if lab.get_rows() > lab.get_cols():
+        w = int(Cnfg.ancho / lab.get_rows())
+    else:
+        w = int(Cnfg.alto / lab.get_cols())
+    y = -w + 20
+
+    for i in range(0, lab.get_rows()):
+        x = 20
+        y = y + w
+
+        for j in range(0, lab.get_cols()):
+            cell = lab.labyrinth[i][j]
+            value = cell.get_value()
+
+            if value == 0:
+                pygame.draw.rect(screen, Cnfg.ASPHALT, [x, y, w, w], 0)
+
+            if value == 1:
+                pygame.draw.rect(screen, Cnfg.GROUND, [x, y, w, w], 0)
+
+            if value == 2:
+                pygame.draw.rect(screen, Cnfg.GRASS, [x, y, w, w], 0)
+
+            if value == 3:
+                pygame.draw.rect(screen, Cnfg.WATER, [x, y, w, w], 0)
+            x = x + w
 
 class Ventana:
-    def dibujarLineas (screen, lab):
-        if lab.get_rows() > lab.get_cols():
-            w = int(Cnfg.ancho / lab.get_rows())
-        else:
-            w = int(Cnfg.alto / lab.get_cols())
-        y = -w + 20
-
-        for i in range(0, lab.get_rows()):
-            x = 20
-            y = y + w
-
-            for j in range(0, lab.get_cols()):
-                cell = lab.labyrinth[i][j]
-                vecinos = cell.get_neighbors()
-
-                if not vecinos[0]:
-                    pygame.draw.line(screen, Cnfg.BLACK, [x, y], [x + w, y])
-
-                if not vecinos[1]:
-                    pygame.draw.line(screen, Cnfg.BLACK, [
-                                     x + w, y], [x + w, y + w])
-
-                if not vecinos[2]:
-                    pygame.draw.line(screen, Cnfg.BLACK, [
-                                     x + w, y + w], [x, y + w])
-
-                if not vecinos[3]:
-                    pygame.draw.line(screen, Cnfg.BLACK, [x, y + w], [x, y])
-                x = x + w
-
-    def dibujarColores(screen, lab):
-        if lab.get_rows() > lab.get_cols():
-            w = int(Cnfg.ancho / lab.get_rows())
-        else:
-            w = int(Cnfg.alto / lab.get_cols())
-        y = -w + 20
-
-        for i in range(0, lab.get_rows()):
-            x = 20
-            y = y + w
-
-            for j in range(0, lab.get_cols()):
-                cell = lab.labyrinth[i][j]
-                value = cell.get_value()
-
-                if value == 0:
-                    pygame.draw.rect(screen, Cnfg.ASPHALT, [x, y, w, w], 0)
-
-                if value == 1:
-                    pygame.draw.rect(screen, Cnfg.GROUND, [x, y, w, w], 0)
-
-                if value == 2:
-                    pygame.draw.rect(screen, Cnfg.GRASS, [x, y, w, w], 0)
-
-                if value == 3:
-                    pygame.draw.rect(screen, Cnfg.WATER, [x, y, w, w], 0)
-
     def dibujar (screen, lab):
         dibujarColores(screen, lab)
         dibujarLineas(screen, lab)
