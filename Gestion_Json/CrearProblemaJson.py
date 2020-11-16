4f2079ec3c96f0800f03560d22602cf67a29441a
sergiomartindg <sergiomartindg@gmail.com>
2020-11-16 12:16:43 +0100

    Añadido un método para obtener el nombre del fichero que se genera

--- Gestion_Json/CrearProblemaJson.py
+++ Gestion_Json/CrearProblemaJson.py
@@ -2,6 +2,7 @@ import os
 import json
 import Cnfg
 
+
 class CrearProblemaJson:
 
     def __init__(self, inicial, final, maze):
@@ -10,9 +11,10 @@ class CrearProblemaJson:
         data["OBJECTIVE"] = final
         data["MAZE"] = maze
 
-        file_name = "Problema_B1_2_{0}to{1}.json".format(inicial, final)
-
+        self.file_name = "Problema_B1_2_{0}to{1}.json".format(inicial, final)
         Cnfg.objetivo = final
-        print("MODIFICADO EL OBJETIVO: " + str(Cnfg.objetivo), flush=True)
-        with open(os.path.join("{0}/JSONs/PROBLEMAs".format(os.getcwd()), file_name), 'w') as file:
+        with open(os.path.join("{0}/Recursos/JSONs/PROBLEMAs".format(os.getcwd()), self.file_name), 'w') as file:
             json.dump(data, file, indent=4)
+
+    def get_nombre_problema(self):
+        return self.file_name
