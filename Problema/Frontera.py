c5a3f362fd3ffc708ea6054f4788508c4c91f82b
sergiomartindg <sergiomartindg@gmail.com>
2020-11-19 19:50:49 +0100

    se renombra el metodo elimina por getPrimerElemento()

--- Problema/Frontera.py
+++ Problema/Frontera.py
@@ -1,16 +1,14 @@
 class Frontera:
-    def CreaFrontera(self):
-        frontera = []
-        return frontera
 
     def __init__(self):
-        self.__listaFrontera = self.CreaFrontera()
+        frontera = []
+        self.__listaFrontera = frontera
 
     def insertar(self, nodoArbol):
         self.__listaFrontera.append(nodoArbol)
         self.__listaFrontera.sort(key=lambda nodoArbol: (nodoArbol.getValor(), nodoArbol.getEstado().getId()[0], nodoArbol.getEstado().getId()[1], nodoArbol.getIdNodo()))
 
-    def elimina(self):  # Devuelve y elimina el  nodo con menos valor
+    def getPrimerElemento(self):  # Devuelve y elimina el  nodo con menos valor
         return self.__listaFrontera.pop(0)
 
     def esVacia(self):
